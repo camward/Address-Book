f2f9414804f632b3ff715870065a66aa5e24f03c
alex <camward@yandex.ru>
2016-02-08 00:09:35 +0400

    @FXML аннотации

--- src/controller/EditController.java
+++ src/controller/EditController.java
@@ -1,5 +1,27 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class EditController {
 
+    @FXML
+    private Button addOk;
+
+    @FXML
+    private Button addCancel;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private Label lblPhone;
+
+    @FXML
+    private Label lblName;
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,18 +1,43 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSrch;
+
+    @FXML
+    private TextField txtSrch;
+
+    @FXML
+    private Label lblCount;
+
     public void showAdd(ActionEvent actionEvent){
         try {
+            btnAdd.setText("Добавление...");
+
             Stage stage = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("../fxml/add.fxml"));
             stage.setTitle("Добавить запись");
